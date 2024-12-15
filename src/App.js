82cf4837b3d41e3e803d84f4c247fea6cc5046a2
Samet <113161657+Xrlnes@users.noreply.github.com>
2024-12-15 19:55:30 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Titles from './components/Titles';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import News from './components/News';
 import CreditShowcase from './components/CreditsShowcase';
+import CreditPage from './components/CreditPage';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <Header />
-      <Hero />
-      <Titles />
-      <News />
-      <CreditShowcase />
-      <Footer  />
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-100">
+        <Header />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Hero />
+              <Titles />
+              <News />
+              <CreditShowcase />
+            </>
+          } />
+          <Route path="/credits" element={<CreditPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
